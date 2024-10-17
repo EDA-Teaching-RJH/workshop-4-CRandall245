1b91827c1522b83fa995022b1290ad1800fcdcdf
CRandall245 <ce327@kent.ac.uk>
2024-10-17 13:28:21 +0000

    commit 1

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,20 @@
+import array
 
+def main():
+    camelCase = input("Give me a string of words in camel case:").strip()
+    z = list(camelCase)
+    i = 0
+    length = len(z)
+    while i < length:
+        Char = z[i]
+        i = i + 1
+        if Char.islower() == True:
+            print(Char,end="")
+        elif Char.isupper() == True:
+            Char = Char.lower()
+            print("_",Char,end="")
+            
+
+        
+
+main()
--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,26 @@
+def main():
+    z = True
+    total = 75
+    while z == True:
+        coins = int(input("Insert a 5 , 10 , 20 , 50 p coin:"))
+        total = total - coins
+        if total > 0:
+            print("There is " , total , "p left")
+        elif total == 0:
+            print("payment accepted")
+            z = False
+        elif total < 0:
+            i = 0
+            while total < 0:
+                total = total + 1
+                i = i + 1
+                if total == 0:
+                    print("you are owed " , i ,"p")
+                    z = False
+                else:
+                    print(f"",end="")
+                    
+                
 
+main()
+        
